186620d8f59df66de4ffe1a1e1b61edd071f025b
erhic <eric.gichovi@student.moringaschool.com>
2022-01-23 08:45:30 +0300

    added a tear down method to do clean up after every test case that runs

--- test_account.py
+++ test_account.py
@@ -15,4 +15,10 @@ class TestAccount(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_account = Account("Abdi","Mustafa","0723232457","abdi@yahoo.com") # create account object
+        
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Account.account_list = []
     
