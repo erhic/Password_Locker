9c88406a884cd26c718063f46586fdcbdf89375c
erhic <eric.gichovi@student.moringaschool.com>
2022-01-23 09:48:09 +0300

    added a function that creates an account

--- run.py
+++ run.py
@@ -3,3 +3,11 @@
 from account import Account
 from credential import Credential
 
+#Creating a Account
+def create_contact(fname,lname,phone,email):
+    
+    '''
+    Function to create a new account
+    '''
+    new_account = Account(fname,lname,phone,email)
+    return new_account
