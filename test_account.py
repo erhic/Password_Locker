af56178c316f3c58ccbcafb09e507d2b181a3168
erhic <eric.gichovi@student.moringaschool.com>
2022-01-23 08:56:31 +0300

    added the second test case ,to test if accountobject is saved or appended in the account list

--- test_account.py
+++ test_account.py
@@ -28,10 +28,18 @@ class TestAccount(unittest.TestCase):
         '''
         test_init test case to test if the object is initialized properly
         '''
-
         self.assertEqual(self.new_account.first_name,"Abdi")
         self.assertEqual(self.new_account.last_name,"Mustafa")
         self.assertEqual(self.new_account.phone_number,"0723232457")
         self.assertEqual(self.new_account.email,"abdi@yahoo.com")
 
+    # ...  Test Case--2
     
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list /appended
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(Account.account_list),1)
+        
