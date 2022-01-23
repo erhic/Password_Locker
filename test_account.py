988d27157711ef0678be63aee5f374a25d678f1e
erhic <eric.gichovi@student.moringaschool.com>
2022-01-23 08:38:06 +0300

    added a class that define the test cases for our account and passed a subclass that enable create test cases as argument

--- test_account.py
+++ test_account.py
@@ -1,2 +1,12 @@
 import unittest # Importing the unittest module
 from account import Account # Importing the contact class
+
+class TestAccount(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the account class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases;subclass
+    '''
+    
